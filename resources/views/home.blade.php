61b2d89f9f0e52c22bd59979acb8bfb3ab488f7c
rspl-ramesh <ramesh.prajapati@rishabhsoft.com>
2017-11-30 12:24:35 +0530

    Dahabord changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    welcomd,You are on dahsboard!
                 </div>
             </div>
         </div>
